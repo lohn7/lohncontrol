e6094b13dccc35544d57a7caa69ce03d92fbcc90
lohn7 <52126698+lohn7@users.noreply.github.com>
2019-06-27 13:07:26 +0300

    Add files via upload

---
+++ web/_includes/headtags.php
@@ -0,0 +1,32 @@
+	<meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Lohn Control : Lohn industry blockchain platform" />
+    <meta property="og:title" content="The processing economy without trust problems" />
+    <meta property="og:url" content="https://lohncontrol.com" />
+    <meta property="og:description" content="Lohncontrol platform is a dynamic and complex ecosystem that will integrate all parties involved into a lohn contract" />
+    <meta property="og:image" content="https://lohncontrol.com/resources/lohncontrol.jpg" />
+    <meta property="og:locale" content="en_EN" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@LOHN09047729" />
+    <meta name="twitter:title" content="Lohncontrol" />    
+    <meta name="twitter:description" content="Lohncontrol platform is a dynamic and complex ecosystem that will integrate all parties involved into a lohn contract" />
+    
+    <link rel="icon" href="icon.png"> 
+    <link href="_assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="_assets/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="_assets/css/icomoon.min.css" />
+    <link rel="stylesheet" href="_assets/css/swiper.min.css" /> 
+    <link rel="stylesheet" href="_assets/css/animate.css" />  
+    <link rel="stylesheet" href="_assets/css/somesvg.css" />
+    <link rel="stylesheet" href="_assets/css/google-fonts.css" /> 
+    <link rel="stylesheet" href="_assets/css/timeline.css" /> 
+    <link rel="stylesheet" media="screen" href="_assets/css/style-particle.css" />  
+    <link rel="stylesheet" href="_assets/css/style.scss" />    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" />
+    
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->    
+    
+    <script  src="_assets/js/constructor.js"></script> 
---
+++ web/_includes/site-scripts.php
@@ -0,0 +1,25 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118972403-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-118972403-1');
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>   
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.1/TweenMax.min.js'></script>            <!-- beeping dots -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'></script> <!-- particles circles -->  
+<script  src="_assets/js/timeline.js"></script> 								<!-- Timeline slider --> 
+<script  src="_assets/js/apps.js"></script> 								    <!-- Header effects --> 
+<script src="_assets/js/swiper.jquery.min.js"></script>                         <!-- Carousel slider -->
+<script src="_assets/js/jquery.mCustomScrollbar.concat.min.js"></script>        <!-- Custom scroll bar -->
+<script src="_assets/js/modernizr-custom.min.js"></script>                      <!-- Modernizr -->
+<script src="_assets/js/sequence.min.js"></script>                              <!-- Header Slider -->
+<script src="_assets/js/jquery.countimator.min.js"></script>                    <!-- Counter -->
+<script src="_assets/js/bootstrap.min.js"></script>                             <!-- Bootstrap -->
+<script src="_assets/js/jquery.sticky.min.js"></script>                         <!-- Sticky Header -->
+<script src="_assets/js/jquery.scrollUp.min.js"></script>                       <!-- scroll top -->
+<script src="_assets/js/wow.min.js"></script>									<!-- Wow animation js -->
+<script src="_assets/js/allof.min.js"></script>									<!-- ALL animation js -->
+<script src="_assets/js/mailer.js"></script>									<!-- WHITELISTMAILER animation js -->
