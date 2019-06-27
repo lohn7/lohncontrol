7723c1866f5fed859fc86065b770bab6dd45b86f
Lohncontrol <vasile.lupu@bitdevelopment.ro>
2019-06-27 14:19:25 +0200

    Create form_validation_lang.php

---
+++ mvp/system/codeigniter/language/romanian/form_validation_lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Open Software License version 3.0
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * licensing@ellislab.com so we can send you a copy immediately.
+ *
+ * @package		CodeIgniter
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2012, EllisLab, Inc. (http://ellislab.com/)
+ * @license		http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+$lang['required']       = "Câmpul %s este obligatoriu.";
+$lang['isset']          = "Câmpul %s trebuie să aibă o valoare.";
+$lang['valid_email']		= "Câmpul %s trebuie să conţină o adresa de email validă.";
+$lang['valid_emails']		= "Câmpul %s trebuie să conţină toate adresele de email valide.";
+$lang['valid_url']			= "Câmpul %s trebuie să conţină un URL valid.";
+$lang['valid_ip']       = "Câmpul %s trebuie să conţină un IP valid.";
+$lang['min_length']			= "Câmpul %s trebuie să aibe cel puţin %s caractere lungime.";
+$lang['max_length']			= "Câmpul %s nu trebuie să depasescă %s caractere lungime.";
+$lang['exact_length']		= "Câmpul %s trebuie să aibe exact %s caractere lungime.";
+$lang['alpha']          = "Câmpul %s poate conţine numai caractere alfabetice.";
+$lang['alpha_numeric']	= "Câmpul %s poate conţine numai caractere alfanumerice.";
+$lang['alpha_dash']			= "Câmpul %s poate conţine numai caractere alfanumerice, underscore, şi liniuţă.";
+$lang['numeric']        = "Câmpul %s poate conţine numai numere.";
+$lang['is_numeric']			= "Câmpul %s poate conţine numai caractere numerice.";
+$lang['integer']        = "Câmpul %s trebuie să conţină un număr întreg.";
+$lang['regex_match']		= "Câmpul %s nu este în formatul corect.";
+$lang['matches']        = "Câmpul %s nu se potriveşte cu campul %s .";
+
+$lang['is_natural']			= "Câmpul %s poate conţine numai numere pozitive.";
+$lang['is_natural_no_zero']	= "Câmpul %s poate conţine numai numere mai mari ca zero.";
+$lang['greater_than']	= "%s trebuie să conţină numai numere mai mari ca %s.";
+$lang['less_than']	= "%s trebuie să conţină numai numere mai mici ca %s.";
+
+$lang['greater_than_equal_to']	= 'Câmpul %s trebuie să conţină un număr mai mare sau egal cu %s.';
+$lang['decimal']				= 'Câmpul %s trebuie să conţină un număr zecimal.';
+$lang['less_than_equal_to']		= 'Câmpul %s trebuie să conţină un număr mai mic sau egal cu %s.';
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
