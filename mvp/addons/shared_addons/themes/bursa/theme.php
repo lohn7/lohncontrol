3119bc5c5fc8b3c1fa3e82e53d9a7391630cdefa
lohn7 <52126698+lohn7@users.noreply.github.com>
2019-06-27 13:59:09 +0300

    Create theme.php

---
+++ mvp/addons/shared_addons/themes/bursa/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Bursa extends Theme {
+
+    public $name = 'Bursa';
+    public $author = 'Bursa';
+    public $author_website = 'http://bursadelohn.ro';
+    public $website = 'http://bursadelohn.ro';
+    public $description = '2-column, fixed; horizontal nav.';
+    public $version = '1.0.0';
+
+}
+
+/* End of file theme.php */
